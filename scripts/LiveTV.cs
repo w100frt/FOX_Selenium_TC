85144786d5f15594e0113f3e10aebf69d9c3beaf
codyphillips5 <codyphill5@gmail.com>
2021-01-28 17:35:57 -0500

    channel name

--- scripts/LiveTV.cs
+++ scripts/LiveTV.cs
@@ -130,7 +130,7 @@ namespace SeleniumProject.Function
 						steps.Clear();
 					}
 					
-					channelName = driver.FindElement("xpath","(//div[contains(@class,'live-on-fox-secondary') or @class='live-tv-channel']//a[contains(@class,'pointer video')])[" + channel + "]").GetAttribute("href").Substring(6).ToUpper;
+					channelName = driver.FindElement("xpath","(//div[contains(@class,'live-on-fox-secondary') or @class='live-tv-channel']//a[contains(@class,'pointer video')])[" + channel + "]").GetAttribute("href").Substring(6).ToUpper();
 					
 					steps.Add(new TestStep(order, "Select Channel " + channel + " - " + channelName, "", "click", "xpath", "(//div[contains(@class,'live-on-fox-secondary') or @class='live-tv-channel']//a[contains(@class,'pointer video')])[" + channel + "]", wait));
 					TestRunner.RunTestSteps(driver, null, steps);
