c6b3d3e4bb0a7170d901dff0dcf9e2a5508b796b
codyphillips5 <codyphill5@gmail.com>
2021-02-18 18:48:51 -0500

    nascar scores

--- scripts/NASCAR_Scores.cs
+++ scripts/NASCAR_Scores.cs
@@ -18,8 +18,11 @@ namespace SeleniumProject.Function
 			string wait = step.Wait != null ? step.Wait : "";
 			List<TestStep> steps = new List<TestStep>();
 			IWebElement ele;
+			DateTime today;
 			string data = "";
+			string xpath = "";
 			VerifyError err = new VerifyError();
+			IJavaScriptExecutor js = (IJavaScriptExecutor)driver.GetDriver();
 
 			string[] nascarGroups = {"CUP SERIES", "GANDER RV & OUTDOORS TRUCK SERIES", "XFINITY SERIES"};
 			
@@ -45,6 +48,40 @@ namespace SeleniumProject.Function
 						err.CreateVerificationError(step, nascarGroups[i], groups[i].GetAttribute("innerText"));
 					}
 				}
+			}
+			
+			else if (step.Name.Equals("Determine Current Race")) {
+				today = DateTime.Today;
+				// determine week of season by today's date and time
+				if (today >= DateTime.Parse("02/01/2021") && today < DateTime.Parse("02/09/2021 11:00:00")) {
+					data = "4469";
+				}
+				else if (today >= DateTime.Parse("02/09/2021 11:00:01") && today < DateTime.Parse("02/15/2020 11:00:00")) {
+					data = "4431";
+				}
+				else if (today >= DateTime.Parse("02/15/2021 11:00:01") && today < DateTime.Parse("02/22/2020 11:00:00")) {
+					data = "4503";
+				}
+				else if (today >= DateTime.Parse("02/22/2021 11:00:01") && today < DateTime.Parse("03/01/2020 11:00:00")) {
+					data = "4465";
+				}
+				else if (today >= DateTime.Parse("03/01/2021 11:00:01") && today < DateTime.Parse("03/08/2020 11:00:00")) {
+					data = "4437";
+				}
+				else if (today >= DateTime.Parse("03/08/2021 11:00:01") && today < DateTime.Parse("03/15/2020 11:00:00")) {
+					data = "4435";
+				}
+				else if (today >= DateTime.Parse("03/15/2021 11:00:01") && today < DateTime.Parse("03/22/2020 11:00:00")) {
+					data = "4463";
+				}
+				else {
+					data = "4436";
+				}		
+				
+				xpath = "//div[contains(@id,'"+ data +"')]";
+				ele = driver.FindElement("xpath", xpath);
+                js.ExecuteScript("arguments[0].scrollIntoView(true);", ele);
+				log.Info("*TEMPORARY FIX* : Scroll to Score Chip " + data);
 			}
 			
 			else {
