51de36e2980281c390d4272a6b1bd20297e3b2ac
codyphillips5 <codyphill5@gmail.com>
2021-02-19 10:45:24 -0500

    nascar scores

---
+++ scripts/IndSport_Scores.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Globalization;
+using System.Collections.Generic;
+using SeleniumProject.Utilities;
+using OpenQA.Selenium;
+using log4net;
+using System.Threading;
+
+namespace SeleniumProject.Function
+{
+	public class Script : ScriptingInterface.IScript
+	{		
+		private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+		public void Execute(DriverManager driver, TestStep step)
+		{
+			long order = step.Order;
+			string wait = step.Wait != null ? step.Wait : "";
+			List<TestStep> steps = new List<TestStep>();
+			IWebElement ele;
+			DateTime today;
+			string data = "";
+			string xpath = "";
+			VerifyError err = new VerifyError();
+			IJavaScriptExecutor js = (IJavaScriptExecutor)driver.GetDriver();
+            OpenQA.Selenium.Interactions.Actions actions = new OpenQA.Selenium.Interactions.Actions(driver.GetDriver());
+
+			if (step.Name.Equals("Verify Event Title")) {
+				xpath = "//div[contains(@id,'"+ DataManager.CaptureMap["IND_EVENTID"] +"')]//div[contains(@class,'scorechip-title')]";
+				steps.Add(new TestStep(order, step.Name, step.Data, "verify_value", "xpath", xpath, wait));
+				TestRunner.RunTestSteps(driver, null, steps);
+				steps.Clear();
+			}
+			
+			else if (step.Name.Equals("Verify Event Track") || step.Name.Equals("Verify Event Course")) {
+				xpath = "//div[contains(@id,'"+ DataManager.CaptureMap["IND_EVENTID"] +"')]//div[contains(@class,'scorechip-sub1')]";
+				steps.Add(new TestStep(order, step.Name, step.Data, "verify_value", "xpath", xpath, wait));
+				TestRunner.RunTestSteps(driver, null, steps);
+				steps.Clear();
+			}
+			
+			else if (step.Name.Equals("Verify Event Location")) {
+				xpath = "//div[contains(@id,'"+ DataManager.CaptureMap["IND_EVENTID"] +"')]//div[contains(@class,'scorechip-sub2')]";
+				steps.Add(new TestStep(order, step.Name, step.Data, "verify_value", "xpath", xpath, wait));
+				TestRunner.RunTestSteps(driver, null, steps);
+				steps.Clear();
+			}			
+			
+			else {
+				throw new Exception("Test Step not found in script");
+			}
+
+		}
+	}
+}
