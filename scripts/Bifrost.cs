6ad4b2a934c74b46b67291fbd2e253a2265b028e
codyphillips5 <codyphill5@gmail.com>
2021-02-19 11:57:36 -0500

    nascar scores

--- scripts/Bifrost.cs
+++ scripts/Bifrost.cs
@@ -29,12 +29,13 @@ namespace SeleniumProject.Function
 			string fileLocation = "https://api.foxsports.com/bifrost/v1/nascar/scoreboard/segment/202102?groupId=2&apikey=jE7yBJVRNAwdDesMgTzTXUUSx1It41Fq";
 			var jsonFile = new WebClient().DownloadString(fileLocation);
 			var json = JObject.Parse(jsonFile);
+			jsonValue = json;
             DataManager.CaptureMap["IND_EVENTID"] = json["currentSectionId"].ToString();
 			log.Info("Current Section ID from Bifrost: " + DataManager.CaptureMap["IND_EVENTID"]);
 			
-			for (int i = 0; i > json["sectionList"].Count; i++) {
-				if (DataManager.CaptureMap["IND_EVENTID"] == json["sectionList"][i].id) {
-					log.Info("HERE. " + DataManager.CaptureMap["IND_EVENTID"] + " equals " + json["sectionList"][i].id);
+			for (int i = 0; i > jsonValue["sectionList"].Count; i++) {
+				if (DataManager.CaptureMap["IND_EVENTID"] == jsonValue["sectionList"][i].id) {
+					log.Info("HERE. " + DataManager.CaptureMap["IND_EVENTID"] + " equals " + jsonValue["sectionList"][i].id);
 				}
 			}
 			
