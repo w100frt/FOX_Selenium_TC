b54e360526e2f39fcc9b9b8573beec82cfc23c81
codyphillips5 <codyphill5@gmail.com>
2021-02-23 10:25:52 -0500

    smokeos

--- scripts/PreviewPass.cs
+++ scripts/PreviewPass.cs
@@ -63,7 +63,7 @@ namespace SeleniumProject.Function
 				byte[] bytes = Encoding.Default.GetBytes(time);
 				time = Encoding.UTF8.GetString(bytes);
 				
-				steps.Add(new TestStep(order, "Verify PVP Countdown Text", time, "verify_value", "xpath", "substring(//div[contains(@class,'pvp-expires')]/span, 1, 19)", wait));
+				steps.Add(new TestStep(order, "Verify PVP Countdown Text", time, "verify_value", "xpath", "substring(//div[contains(@class,'pvp-expires')]/span, '1', '19')", wait));
 				TestRunner.RunTestSteps(driver, null, steps);
 				steps.Clear();
 			}
