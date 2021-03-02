6e3a8391796f9740bfb9030fe336da1217c5ef7c
Lydia Wachtman <lydia.wachtman@fox.com>
2021-03-02 12:42:13 -0500

    Episodes

--- scripts/Shows.cs
+++ scripts/Shows.cs
@@ -34,7 +34,7 @@ namespace SeleniumProject.Function
 
 			if (step.Name.Equals("Capture Episode Title"))
             {
-				WebElement eTitle = driver.FindElements("xpath","(//div[contains(@class,'pdg-top-20')])[0]");
+				string eTitle = driver.FindElements("xpath","(//div[contains(@class,'pdg-top-20')])[0]").getText();
 				log.Info("Episode Title: " + eTitle);
             }
 			
