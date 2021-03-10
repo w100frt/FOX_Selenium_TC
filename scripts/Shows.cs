a2b0f538e83b9c8e62dd9981e53f617e31ea7556
Lydia Wachtman <lydia.wachtman@fox.com>
2021-03-10 13:15:47 -0500

    Episodes

--- scripts/Shows.cs
+++ scripts/Shows.cs
@@ -43,7 +43,10 @@ namespace SeleniumProject.Function
 			else if (step.Name.Equals("Capture Number of Additional Episodes"))
 			{
 				size = driver.FindElements("xpath", "//div[contains(@class,'video-overlay')]").Count;
-
+				if(size == 0)
+                {
+					log.Infor("***THERE ARE NO EPIOSODES POSTED***")
+                }
 				DataManager.CaptureMap["EPISODES"] = size.ToString();
 				log.Info("Number of additional episodes: " + size);
 			}
